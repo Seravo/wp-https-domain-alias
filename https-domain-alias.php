853943adcf61adc3686543507b8d43303cd842a8
Antti Kuosmanen <antti@zuigeproductions.fi>
2014-06-09 12:45:39 +0300

    Simplified logic + revert indentation style

--- https-domain-alias.php
+++ https-domain-alias.php
@@ -10,20 +10,20 @@
  */
 
 
-/**	Copyright 2014  Otto Kekäläinen / Seravo Oy
+/** Copyright 2014  Otto Kekäläinen / Seravo Oy
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 3, as
-	published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 3, as
+  published by the Free Software Foundation.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 
@@ -54,46 +54,54 @@
  */
 function _https_domain_rewrite($url, $status = 0) {
 
-	//debug: error_log("status=$status");
-	//debug: error_log("url-i=$url");
+  //debug: error_log("status=$status");
+  //debug: error_log("url-i=$url");
 
-	// TODO: second parameter if ofen scheme,
-	//       see http://codex.wordpress.org/Function_Reference/site_url#Parameters
+  // TODO: second parameter if ofen scheme,
+  //       see http://codex.wordpress.org/Function_Reference/site_url#Parameters
 
-	// Assume domain is always same for all calls to this function
-	// during same request and thus define some variables as static.
-	static $domain;
-	if (!isset($domain)) {
-		$domain = parse_url(get_option('home'), PHP_URL_HOST);
-		//debug: error_log("domain=$domain");
-	}
 
-	static $domainAlias;
-	if (!isset($domainAlias)) {
-		 if (substr(HTTPS_DOMAIN_ALIAS, -strlen($domain)) == $domain) {
-			// Special case: $domainAlias ends with $domain,
-			// which is possible in WP Network when requesting
-			// the main site, don't rewrite urls as a https
-			// certificate for sure exists for direct domain.
-			// e.g. domain seravo.fi, domain alias *.seravo.fi
-			$domainAlias = $domain;
-		} else if (substr(HTTPS_DOMAIN_ALIAS, 0, 1) == '*') {
-			$domainBase = substr($domain, 0, strpos($domain, '.'));
-			$domainAliasBase = substr(HTTPS_DOMAIN_ALIAS, 1);
-			$domainAlias = $domainBase . $domainAliasBase;
-		} else {
-			$domainAlias = HTTPS_DOMAIN_ALIAS;
-		}
-		//debug: error_log("domainAlias=$domainAlias");
-	}
+  // Rewrite only if the request is https, or the user is logged in
+  // to preserve cookie integrity
+  if (substr($url, 0, 5) == 'https' || ( function_exists('is_user_logged_in') && is_user_logged_in() )) {
 
-	// If $location does not include simple https domain alias, rewrite it.
-	if ($domain != $domainAlias) {
-		$url = str_ireplace($domain, $domainAlias, $url);
-		//debug: error_log("url-o=$url");
-	}
+      // Assume domain is always same for all calls to this function
+      // during same request and thus define some variables as static.
+      static $domain;
+      if (!isset($domain)) {
+        $domain = parse_url(get_option('home'), PHP_URL_HOST);
+        //debug: error_log("domain=$domain");
+      }
 
-	return $url;
+      static $domainAlias;
+      if (!isset($domainAlias)) {
+         if (substr(HTTPS_DOMAIN_ALIAS, -strlen($domain)) == $domain) {
+          // Special case: $domainAlias ends with $domain,
+          // which is possible in WP Network when requesting
+          // the main site, don't rewrite urls as a https
+          // certificate for sure exists for direct domain.
+          // e.g. domain seravo.fi, domain alias *.seravo.fi
+          $domainAlias = $domain;
+        } else if (substr(HTTPS_DOMAIN_ALIAS, 0, 1) == '*') {
+          $domainBase = substr($domain, 0, strpos($domain, '.'));
+          $domainAliasBase = substr(HTTPS_DOMAIN_ALIAS, 1);
+          $domainAlias = $domainBase . $domainAliasBase;
+        } else {
+          $domainAlias = HTTPS_DOMAIN_ALIAS;
+        }
+        //debug: error_log("domainAlias=$domainAlias");
+      }
+
+      // If $location does not include simple https domain alias, rewrite it.
+      if ($domain != $domainAlias) {
+        $url = str_ireplace($domain, $domainAlias, $url);
+        $url = str_replace('http://', 'https://', $url);
+        //debug: error_log("url-o=$url");
+      }
+
+  }
+
+  return $url;
 }
 
 
@@ -107,10 +115,10 @@ function _https_domain_rewrite($url, $status = 0) {
  * @return string $url
  */
 function _debug_rewrite($url, $path=false, $plugin=false, $extra=false) {
-	error_log("url=$url");
-	$url = _https_domain_rewrite($url);
-	error_log("return=$url");
-	return $url;
+  error_log("url=$url");
+  $url = _https_domain_rewrite($url);
+  error_log("return=$url");
+  return $url;
 }
 
 
@@ -119,13 +127,13 @@ function _debug_rewrite($url, $path=false, $plugin=false, $extra=false) {
  */
 function _home_url_rewrite($url) {
 
-	//don't rewrite urls for polylang settings
-	if($_GET['page'] == 'mlang') {
-		return $url;
-	}
+  //don't rewrite urls for polylang settings
+  if($_GET['page'] == 'mlang') {
+    return $url;
+  }
 
-	$url = _https_domain_rewrite($url);
-	return $url;
+  $url = _https_domain_rewrite($url);
+  return $url;
 }
 
 
@@ -134,33 +142,20 @@ function _home_url_rewrite($url) {
  */
 if (defined('HTTPS_DOMAIN_ALIAS')) {
 
-	// A redirect or link to https may happen from pages served via http
-	add_filter('login_url', '_https_domain_rewrite');
-	add_filter('logout_url', '_https_domain_rewrite');
-	add_filter('admin_url', '_https_domain_rewrite');
-
-	// These are only needed if site is already accessed via https or if the user is logged in
-	function _ssl_redirects() {
-		if(!function_exists('is_user_logged_in') || is_user_logged_in()) {
-			add_filter('wp_redirect', '_https_domain_rewrite');
-			add_filter('plugins_url', '_https_domain_rewrite');
-			add_filter('content_url', '_https_domain_rewrite');
-			add_filter('site_url', '_https_domain_rewrite');
-			add_filter('home_url', '_home_url_rewrite');
-		}
-	}
-
-	if (is_ssl()) {
-		// We're on HTTPS, do redirects directly
-		_ssl_redirects();
-	}
-	else {
-		// We need to wait for init to check user login status
-		add_action('init', '_ssl_redirects');
-	}
+  // A redirect or link to https may happen from pages served via http
+  add_filter('login_url', '_https_domain_rewrite');
+  add_filter('logout_url', '_https_domain_rewrite');
+  add_filter('admin_url', '_https_domain_rewrite');
+  add_filter('wp_redirect', '_https_domain_rewrite');
+  add_filter('plugins_url', '_https_domain_rewrite');
+  add_filter('content_url', '_https_domain_rewrite');
+  add_filter('site_url', '_https_domain_rewrite');
+  add_filter('home_url', '_home_url_rewrite');
 
 } else {
-	error_log('Constant HTTPS_DOMAIN_ALIAS is not defined');
+
+  error_log('Constant HTTPS_DOMAIN_ALIAS is not defined');
+
 }
 
 
@@ -175,17 +170,17 @@ if (defined('HTTPS_DOMAIN_ALIAS')) {
  */
 add_action('activated_plugin', 'https_domain_alias_must_be_first_plugin');
 function https_domain_alias_must_be_first_plugin() {
-	// ensure path to this file is via main wp plugin path
-	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
-	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
-	$active_plugins = get_option('active_plugins');
-	$this_plugin_key = array_search($this_plugin, $active_plugins);
+  // ensure path to this file is via main wp plugin path
+  $wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+  $this_plugin = plugin_basename(trim($wp_path_to_this_file));
+  $active_plugins = get_option('active_plugins');
+  $this_plugin_key = array_search($this_plugin, $active_plugins);
 
-	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
-		array_splice($active_plugins, $this_plugin_key, 1);
-		array_unshift($active_plugins, $this_plugin);
-		update_option('active_plugins', $active_plugins);
-	}
+  if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+    array_splice($active_plugins, $this_plugin_key, 1);
+    array_unshift($active_plugins, $this_plugin);
+    update_option('active_plugins', $active_plugins);
+  }
 }
 
 
@@ -194,10 +189,10 @@ function https_domain_alias_must_be_first_plugin() {
  */
 add_action('admin_menu', '_https_domain_alias_readme');
 function _https_domain_alias_readme() {
-	//Readme is only visible, when HTTPS_DOMAIN_ALIAS is not defined
-	if(!defined('HTTPS_DOMAIN_ALIAS')) {
-		add_options_page('HTTPS Domain Alias', 'HTTPS Domain Alias', 'administrator', __FILE__, '_build_readme_page', plugins_url('/images/icon.png', __FILE__));
-	}
+  //Readme is only visible, when HTTPS_DOMAIN_ALIAS is not defined
+  if(!defined('HTTPS_DOMAIN_ALIAS')) {
+    add_options_page('HTTPS Domain Alias', 'HTTPS Domain Alias', 'administrator', __FILE__, '_build_readme_page', plugins_url('/images/icon.png', __FILE__));
+  }
 }
 
 function _build_readme_page() {
@@ -205,15 +200,15 @@ function _build_readme_page() {
 
 <div class="wrap">
 
-	<h2>HTTPS Domain Alias</h2>
+  <h2>HTTPS Domain Alias</h2>
 
-	<div id="message" class="error"><p><?php _e('This readme page is only visible when HTTPS_DOMAIN_ALIAS is not defined in wp-config.php. You will not see this once the constant is defined.');?> </p></div>
+  <div id="message" class="error"><p><?php _e('This readme page is only visible when HTTPS_DOMAIN_ALIAS is not defined in wp-config.php. You will not see this once the constant is defined.');?> </p></div>
 
-	<?php include('readme.html'); ?>
+  <?php include('readme.html'); ?>
 
-	<p>&nbsp;</p>
+  <p>&nbsp;</p>
 
-	<p><small>HTTPS Domain Alias is made by <a href="http://seravo.fi/">Seravo Oy</a>, which specializes in open source support services and among others is the only company in Finland to provide [WordPress Premium Hosting](http://seravo.fi/wordpress-palvelu).</small></p>
+  <p><small>HTTPS Domain Alias is made by <a href="http://seravo.fi/">Seravo Oy</a>, which specializes in open source support services and among others is the only company in Finland to provide [WordPress Premium Hosting](http://seravo.fi/wordpress-palvelu).</small></p>
 
 </div>
 <?php
